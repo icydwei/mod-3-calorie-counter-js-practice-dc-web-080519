526991e620b07a71c84527214c8b0edc36054b56
icydwei <robpaik@gmail.com>
2019-09-24 11:32:39 -0400

    progress bar updates on added entry - delete button feature complete, also removes from progress bar when removed

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 // your code here, it may be worth it to ensure this file only runs AFTER the dom has loaded.
-const URL = "http://localhost:3000/api/v1/calorie_entries"
+const URL = "http://localhost:3000/api/v1/calorie_entries";
+let totCal = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
   fetchCalorieEntries()
   newCalEntryForm()
+  calListBtns()
 })
 
 //Fetch calorie entries
@@ -26,18 +28,21 @@ function renderEntry(calorieEntry) {
 //Extracted out code to be used for prepending new entries as well
 function createEntry(calorieEntry) {
   let li = document.createElement("li")
+  let progBar = document.querySelector(".uk-progress")
+  totCal += calorieEntry.calorie
+  progBar.value = totCal
   li.classList.add("calories-list-item")
-  li.innerHTML = `<div class="uk-grid"> <div class="uk-width-1-6"> <strong>${calorieEntry.calorie}</strong> <span>kcal</span> </div> <div class="uk-width-4-5"> <em class="uk-text-meta">${calorieEntry.note}</em> </div> </div> <div class="list-item-menu"> <a class="edit-button" uk-toggle="target: #edit-form-container" uk-icon="icon: pencil"></a> <a class="delete-button" uk-icon="icon: trash"></a> </div>`
+  li.innerHTML = `<div class="uk-grid" id="entry-${calorieEntry.id}"> <div class="uk-width-1-6"> <strong>${calorieEntry.calorie}</strong> <span>kcal</span> </div> <div class="uk-width-4-5"> <em class="uk-text-meta">${calorieEntry.note}</em> </div> </div> <div class="list-item-menu"> <a class="edit-button" uk-toggle="target: #edit-form-container" uk-icon="icon: pencil"></a> <a class="delete-button" uk-icon="icon: trash"></a> </div>`
   return li
 }
 
 //Allow users to submit new calorie entries - entries should be pre-pended
 function newCalEntryForm() {
   let form = document.getElementById("new-calorie-form")
-  form.addEventListener("submit", createNewEntry)
+  form.addEventListener("submit", createNewCalEntry)
 }
 
-function createNewEntry(e) {
+function createNewCalEntry(e) {
   e.preventDefault()
   let calNum = e.currentTarget.elements[0].value
   let noteDesc = e.currentTarget.elements[1].value
@@ -62,10 +67,52 @@ function createNewEntry(e) {
   .catch(error => {
     alert("BAZINGA!")
   })
+  e.target.reset()
 }
 
 function prependEntry(calorieEntry) {
   let calList = document.getElementById('calories-list')
   let li = createEntry(calorieEntry)
   calList.prepend(li)
+}
+
+//Cal List Edit & Delete Buttons
+function calListBtns() {
+  let calList = document.getElementById("calories-list")
+  calList.addEventListener("click", handleCalListClicks)
+}
+
+function handleCalListClicks(e) {
+  if (e.target.dataset.svg === "trash"){
+    deleteCalEntry(e)
+  }
+}
+
+function deleteCalEntry(e) {
+  let li = e.target.parentElement.parentElement.parentElement
+  // let cal = e.target.parentElement.parentElement.parentElement.children[0].children[0].innerText.split(" ")[0]
+  // let note = e.target.parentElement.parentElement.parentElement.children[0].children[1].innerText
+  let delId = e.target.parentElement.parentElement.parentElement.children[0].id.split("-")[1]
+
+  fetch(URL + '/' + delId, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }
+  })
+  .then(response => response.json())
+  .then(removedEntry => {
+    deleteEntry(removedEntry, e)
+  })
+  .catch(error => {
+    alert("BAZINGA!")
+  })
+}
+
+function deleteEntry(removedEntry, e) {
+  let progBar = document.querySelector(".uk-progress")
+  totCal -= removedEntry.calorie
+  progBar.value = totCal
+  e.target.parentElement.parentElement.parentElement.remove()
 }
